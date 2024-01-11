58af473823c02bf88b35e0a61cb8d4fd88a102e5
Emmanuelle Laguna Marín <emmanuelle.laguna@gmail.com>
2024-01-11 15:31:34 -0600

    delete: console.log

--- screens/AllPhotos/index.tsx
+++ screens/AllPhotos/index.tsx
@@ -6,13 +6,10 @@ import {getAllPhotos} from '../../lib/api/albums';
 
 export default function AllPhotos() {
   const [photos, setPhotos] = useState([]);
-  console.log('yeah!!!');
 
   useEffect(() => {
     getAllPhotos().then(data => setPhotos(data));
   }, []);
-
-  console.log('photos', photos);
 
   return <Grid photos={photos} />;
 }
